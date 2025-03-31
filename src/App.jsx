4465f278b2725d223036b9205233685d7fd8dba7
Safi100 <safinafi12@gmail.com>
2025-03-31 15:57:10 +0200

    more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useUser, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useUser, SignedIn, SignedOut } from "@clerk/clerk-react";
 
-import SignIn from "./pages/Signin";
-import SignUp from "./pages/Signup";
-import Home from "./pages/Home";
+import SignIn from "./pages/auth/Signin";
+import SignUp from "./pages/auth/Signup";
+import Home from "./pages/home/Home";
+import Tags from "./pages/tags/Tags";
 
-import { MobileNav } from "./components/MobileNav";
+import MobileNav from "./components/MobileNav";
+import MobileHeader from "./components/MobileHeader";
+import SideBar from "./components/SideBar";
+import { getAllTags } from "./db/queries";
 
 function App() {
   const { user } = useUser();
+  const [tags, setTags] = useState([]);
   // hide nav on /sign-in and /sign-up
   const hideNav =
     location.pathname.startsWith("/sign-in") ||
     location.pathname.startsWith("/sign-up");
 
+  useEffect(() => {
+    const fetchTags = async () => {
+      const tags = await getAllTags(user.id);
+      setTags(tags);
+    };
+    fetchTags();
+  }, [user]);
+
   return (
-    <>
-      {!hideNav && (
-        <SignedIn>
-          <MobileNav />
-        </SignedIn>
-      )}
+    <div>
+      <SignedIn>
+        <MobileHeader />
+        <SideBar tags={tags} />
+        <MobileNav />
+      </SignedIn>
+      {/* )} */}
       <Routes>
         <Route
           path="/"
@@ -36,9 +51,10 @@ function App() {
           }
         />
         <Route path="/sign-up/*" element={<SignUp />} />
-        <Route path="/sign-in/*" element={<SignIn />} />
+        <Route path="/sign-up/*" element={<SignUp />} />
+        <Route path="/tags" element={<Tags user={user} />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
