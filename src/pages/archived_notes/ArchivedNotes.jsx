04afb5eb54bf3e4be111c4ec9d036d2d81b9d28c
Safi100 <safinafi12@gmail.com>
2025-04-05 00:57:15 +0200

    do search on archived page

--- src/pages/archived_notes/ArchivedNotes.jsx
+++ src/pages/archived_notes/ArchivedNotes.jsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { fetchAllArchivedNotes } from "../../db/queries";
+import { useSearchParams } from "react-router-dom";
+import { fetchAllArchivedNotes, searchArchivedNotes } from "../../db/queries";
 import NotesBar from "../../components/NotesBar";
 
 const ArchivedNotes = ({ user }) => {
   const [archivedNotes, setArchivedNotes] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("q") || "";
+
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchArchivedNotes = async () => {
+      const results = await searchArchivedNotes(user.id, query);
+      setArchivedNotes(results);
+    };
+
+    fetchArchivedNotes();
+  }, [query, user]);
 
   useEffect(() => {
     const getArchivedNotes = async () => {
