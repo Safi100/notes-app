ee7402a27d2459204e649972e4feeef0d72dcae8
Safi100 <safinafi12@gmail.com>
2025-04-05 20:16:49 +0200

    Create logout function

--- src/components/SettingsBar.jsx
+++ src/components/SettingsBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./components.css";
 import { Link } from "react-router-dom";
+import { useClerk } from "@clerk/clerk-react";
 
 const SettingsBar = () => {
+  const { signOut } = useClerk(); // Get the signOut method from Clerk
+
+  const handleLogout = () => {
+    signOut(); // Call the signOut method to log out the user
+  };
+
   return (
     <div className="settings_bar">
       <p>Settings</p>
@@ -135,9 +142,7 @@ const SettingsBar = () => {
               d="M21 11.998H8.945m12.055 0-2.932-2.934M21 11.998l-2.932 2.936M14.556 8.266V7.251c0-1.56-1.121-2.891-2.651-3.15L6.702 3.046C4.765 2.718 3 4.219 3 6.195v11.61c0 1.976 1.765 3.477 3.702 3.15l5.203-1.057a3.188 3.188 0 0 0 2.65-3.149v-1.014"
             ></path>
           </svg>
-          <span class="text-preset-4 text-neutral-950 dark:text-white">
-            Logout
-          </span>
+          <span onClick={handleLogout}>Logout</span>
         </button>
       </div>
     </div>
