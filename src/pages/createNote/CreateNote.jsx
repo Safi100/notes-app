829d0049be0f6636733ef813abfb84f68a35996e
Safi100 <safinafi12@gmail.com>
2025-04-12 05:15:16 +0300

    update create new note

--- src/pages/createNote/CreateNote.jsx
+++ src/pages/createNote/CreateNote.jsx
@@ -1,41 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { fetchAllNotes, searchNotes, addNote } from "../../db/queries";
+import { useEffect, useState, useContext } from "react";
 import NotesBar from "../../components/NotesBar";
+import { NotesContext } from "../../context/NotesContext";
 import toast, { Toaster } from "react-hot-toast";
 import "./createNote.css";
 
 const CreateNote = ({ user }) => {
+  const notesContext = useContext(NotesContext);
+
   const notify = () => toast.success("Note created successfully!");
 
-  const [searchParams, setSearchParams] = useSearchParams();
   const [formData, setFormData] = useState({
     title: "",
     tags: "",
     content: "",
   });
-  const [notes, setNotes] = useState([]);
-
-  const query = searchParams.get("q") || "";
-
-  useEffect(() => {
-    if (!user) return;
-
-    const fetchNotes = async () => {
-      const results = await searchNotes(user.id, query);
-      setNotes(results);
-    };
-
-    fetchNotes();
-  }, [query, user]);
 
   useEffect(() => {
     const getNotes = async () => {
-      const notes = await fetchAllNotes(user.id);
-      setNotes(notes);
-      console.log(notes);
+      await notesContext.fetch_All_Notes(user.id);
     };
-
     getNotes();
   }, [user]);
 
@@ -58,10 +41,9 @@ const CreateNote = ({ user }) => {
       return;
     }
 
-    const newNote = await addNote(formData, user);
-
+    const newNote = await notesContext.createNote(formData, user.id);
+    console.log(newNote);
     if (newNote) {
-      setNotes([newNote, ...notes]);
       setFormData({ title: "", tags: "", content: "" });
       notify();
     }
@@ -70,7 +52,7 @@ const CreateNote = ({ user }) => {
   return (
     <div className="create_note_container">
       <Toaster position="bottom-right" reverseOrder={false} />
-      <NotesBar notes={notes} />
+      <NotesBar notes={notesContext.notes} />
       <div className="create_note_row">
         <form className="create_note_form" onSubmit={handleSubmit}>
           <div className="note_title_tag">
@@ -95,18 +77,18 @@ const CreateNote = ({ user }) => {
                   >
                     <path
                       stroke="#0E121B"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.8"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.8"
                       className="dark:stroke-white"
                       d="M3.016 5.966c.003-1.411 1.07-2.677 2.456-2.916.284-.05 3.616-.042 4.995-.041 1.364 0 2.527.491 3.49 1.452 2.045 2.042 4.088 4.085 6.128 6.13 1.208 1.21 1.224 3.066.022 4.28a805.496 805.496 0 0 1-5.229 5.228c-1.212 1.201-3.069 1.186-4.279-.022-2.064-2.058-4.127-4.115-6.182-6.182-.795-.8-1.264-1.766-1.368-2.895-.084-.903-.035-4.26-.033-5.034Z"
                       clipRule="evenodd"
                     ></path>
                     <path
                       stroke="#0E121B"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.8"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.8"
                       className="dark:stroke-white"
                       d="M9.907 8.315a1.607 1.607 0 0 1-1.61 1.583c-.872-.002-1.599-.73-1.594-1.596a1.604 1.604 0 0 1 1.633-1.607c.864.003 1.575.736 1.571 1.62Z"
                       clipRule="evenodd"
