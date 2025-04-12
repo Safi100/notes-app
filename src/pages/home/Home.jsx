1657edbda6f9154130db49f54ca771cabf45d71c
Safi100 <safinafi12@gmail.com>
2025-04-12 04:48:38 +0300

    Update home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,37 +1,19 @@
-import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-import { fetchAllNotes, searchNotes } from "../../db/queries";
+import { useState, useEffect, useContext } from "react";
+import { NotesContext } from "../../context/NotesContext";
 import NotesBar from "../../components/NotesBar";
 import "./home.css";
 
 const Home = ({ user }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [notes, setNotes] = useState([]);
-
-  const query = searchParams.get("q") || "";
-
-  useEffect(() => {
-    if (!user) return;
-
-    const fetchNotes = async () => {
-      const results = await searchNotes(user.id, query);
-      setNotes(results);
-    };
-
-    fetchNotes();
-  }, [query, user]);
+  const notesContext = useContext(NotesContext);
 
   useEffect(() => {
     const getNotes = async () => {
-      const notes = await fetchAllNotes(user.id);
-      setNotes(notes);
-      console.log(notes);
+      await notesContext.fetch_All_Notes(user.id);
     };
-
     getNotes();
   }, [user]);
 
-  return <NotesBar notes={notes} />;
+  return <NotesBar notes={notesContext.notes} />;
 };
 
 export default Home;
