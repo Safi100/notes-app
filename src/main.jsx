93861e15582f9b703809f252a73fa3b22354b2b8
Safi100 <safinafi12@gmail.com>
2025-04-12 07:58:57 +0300

    NotesContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import { NotesContextProvider } from "./context/NotesContext";
 import { ClerkProvider } from "@clerk/clerk-react";
+import "./index.css";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,7 +17,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
+        <NotesContextProvider>
+          <App />
+        </NotesContextProvider>
       </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
